f0fbed10665331ea07abf2b861d325c344d745e1
TheShubhamRaj <rajshubham9934@gmail.com>
2025-03-08 16:06:48 +0530

    conversionOpersations

--- 01_basics/03_conversioOperations.js
+++ 01_basics/03_conversioOperations.js
@@ -16,3 +16,36 @@ let somenumber = 33
 let stringNumber = String(somenumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+// ******************** operations**************
+
+let value = 3
+let negative = -value
+// console.log(negative)
+
+// console.log(2+2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+// let str1 = "hello"
+// let str2 = " shubham"
+// let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1" +2);
+// console.log(1+"2")
+// console.log("1"+2 +2)
+// console.log(1 +2+"2")
+
+// console.log(3 +4 *5%3)
+// please watch english channel video
+
+console.log(+true);// conversion
+console.log(+"")// op = 0
+
+let num1,num2,num3
+num1 = num2 =num3 = 2+2
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
