48a80a1959172124e826866d42b717717e53c6c0
TheShubhamRaj <rajshubham9934@gmail.com>
2025-03-31 16:45:27 +0530

    dekhlena

---
+++ 03_basics/05_jsExectionContext.js
@@ -0,0 +1,9 @@
+// Javascript Execution Context
+// {} - global execution context - this 
+
+// js is single threaded
+// funtion execution context
+// eval execution context
+
+//{} - memory creation phase
+// 2. execution phase
