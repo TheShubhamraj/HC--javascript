fca0637c74d07061b3b9ab84ba20996c4ef9bf31
TheShubhamRaj <rajshubham9934@gmail.com>
2025-03-31 17:27:07 +0530

    while and do while loop

---
+++ 05_iterators/two.js
@@ -0,0 +1,17 @@
+let index = 0
+while (index <= 10) {
+    console.log(`value of index is ${index}`);
+    index++
+    
+}
+
+
+// do while loop
+
+let score =11;
+do {
+    console.log(score);
+    
+    score++;
+
+} while (score <=10);
