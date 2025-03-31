093f2d3d1c671cbab0333ffd8db9f85e9579e2e7
TheShubhamRaj <rajshubham9934@gmail.com>
2025-03-31 21:40:13 +0530

    importand foreach loop

--- 05_iterators/four.js
+++ 05_iterators/four.js
@@ -16,3 +16,37 @@ for (const key in programming) {
     console.log(key ,"value of at index is =",programming[key]);
     
 }
+// programming.forEach( (item) => {
+//     console.log(item);
+    
+// })
+
+// function printme(item){
+//     console.log(item);
+    
+// }
+// programming.forEach(printme)// give only reference of function donot call a function
+
+programming.forEach((item,index,arr) => {
+    console.log(item,index,arr);
+    
+})
+
+const mycoding = [
+    {
+        lang:"javascript",
+        langfilename:"js"
+    },
+    {
+        lang:"java",
+        langfilename:"java"
+    },
+    {
+        lang:"python",
+        langfilename:"py"
+    }
+]
+mycoding.forEach((item) =>{
+    console.log(item.lang);
+    
+})
