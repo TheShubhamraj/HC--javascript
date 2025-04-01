610711936739568f87551a418a838b6c735dd3e8
TheShubhamRaj <rajshubham9934@gmail.com>
2025-04-01 11:28:38 +0530

    for each loop and call back function ,return

---
+++ six.js
@@ -0,0 +1,28 @@
+// const coding =["js","ruby",'java',"python","cpp"]
+
+// const value = coding.forEach((item) =>{
+//     console.log(item);
+//     return item;
+// })
+// for each loop does not return any thing or returns undefined
+
+const num = [1,2,3,4,5,6,7,8,9,10]
+// const nums =num.filter((numss) => numss > 4 )
+// const nums =num.filter((numss) =>{
+//     return numss > 4
+// })
+// if you open scope then we have to use return keyword otherwise empty arr will get returned
+
+// const newNums = []
+
+// num.forEach( (nums) => {
+//     if (nums > 4) {
+//         newNums.push(nums)
+//     }
+// })
+
+
+
+// console.log(newNums);
+
+
