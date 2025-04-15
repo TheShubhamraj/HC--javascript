92cdcdda7866e9b9eca6522f39d978b52754d0b0
TheShubhamRaj <rajshubham9934@gmail.com>
2025-04-15 12:27:49 +0530

    preventdefautl use case

--- 07_project2/code.js
+++ 07_project2/code.js
@@ -3,6 +3,8 @@ const form = document.querySelector('form')
 
 form.addEventListener('submit',function(e){
     e.preventDefault();
+
+    // preventing the browser from doing usual work or submitting 
     
    const height = parseInt(document.querySelector('#height').value)
    const weight = parseInt(document.querySelector('#weight').value)
