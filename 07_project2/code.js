e08221811937b8bd896bb22f9a72ddffcf19ceed
TheShubhamRaj <rajshubham9934@gmail.com>
2025-04-15 12:01:34 +0530

    bmi calculator

---
+++ 07_project2/code.js
@@ -0,0 +1,37 @@
+const form = document.querySelector('form')
+
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    
+   const height = parseInt(document.querySelector('#height').value)
+   const weight = parseInt(document.querySelector('#weight').value)
+   const results =document.querySelector('#results')
+
+
+    if(height =='' || height < 0|| weight =='' || weight <= 0 || isNaN(height)){
+        results.innerHTML  = "Please give a valid weight or height";
+    }
+
+    else{
+        const bmi = (weight / ((height*height)/10000)).toFixed(2)
+    // show the result
+    results.innerHTML = `<span>${bmi}</span>`;
+
+
+    if (bmi < 18.6) {
+        results.innerHTML = `you are undernourished`
+    }
+
+else if ( bmi <= 24.9|| bmi >= 18.6) {
+        results.innerHTML = `you are fit congrats`
+    }
+    else {
+        results.innerHTML = `do some work on your body overweight`
+    }
+    }
+})
+
+
+
+// project 2 done successfully 
